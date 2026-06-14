1c6e56901c811eae1e518d7edd1a68c7ec1e8f46
leelohoon <wldn5328@gmail.com>
2026-06-14 13:11:53 +0900

    Restore Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/EDABible/',
   define: {
     __BUILD__: JSON.stringify(pkg.version),
   },
